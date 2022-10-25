3543a93d33334a54cbe44a379b46d8d02cededcd
Baptiste Azevedo <azevedo.baptiste@orange.fr>
2022-10-25 11:22:49 +0200

    first commit

---
+++ back/app.js
@@ -0,0 +1,28 @@
+const express = require ('express')
+const app = express()
+const mongoose = require('mongoose')
+const path = require('path');
+const userRoutes = require('./routes/userRoutes')
+// import environement variables
+require('dotenv').config();
+
+app.use(express.json()); // Remplace bodyparser // 
+
+mongoose.connect(process.env.database_url,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+//app.use('/api/books', booksRoutes) // Routes par défaut des livres
+app.use('/api/auth', userRoutes) // Routes par défaut pour l'inscription/connection
+//app.use('/images', express.static(path.join(__dirname, 'images'))) // Permet l'acces a la ressource statique image
+
+module.exports = app 
